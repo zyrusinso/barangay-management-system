58cdb619c13a7c7e1db7ecc4bdf60216fe50dad2
zyrusinso <zyrusinso@gmail.com>
2025-03-26 13:10:36 +0800

    removing the User Roles

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ use Spatie\Permission\Traits\HasRoles;
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, HasRoles;
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
